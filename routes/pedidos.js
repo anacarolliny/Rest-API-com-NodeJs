66a60350d5ec48cbb589ac35a9de3ddc76c2287a
anacarolliny <acss2011@hotmail.com>
2021-10-07 10:57:39 -0300

    Adicionando funcionalidades na API

--- routes/pedidos.js
+++ routes/pedidos.js
@@ -1,51 +1,148 @@
 const express = require("express")
 const router = express.Router()//aqui declarando que é uma rota do express
-
+const mysql = require("../mysql").pool
 
 // RETORNA TODOS OS PRODUTOS
 router.get("/", (req, res, next) => { //retorna todos os pedidos
-    res.status(200).send({
-        mensagem: "Retorna os pedidos"
+    mysql.getConnection((error, conn) => {
+        if (error) { return res.status(500).send({ error: error }) } // retorna um erro se nao conseguir acessar a conexão
+        conn.query(
+            "SELECT * FROM pedidos",
+            (error, result, fields) => {
+                if (error) { return res.status(500).send({ error: error }) }
+                const response = { // Aqui posso documentar meu método Get melhor, boas praticas
+                    quantidade: result.length,
+                    pedidos: result.map(pedido => { // no array e produtos, cada item dele vou modificar o valor, vai ter esse padrão
+                        return {
+                            id_pedido: pedido.id_pedido,
+                            id_produto: pedido.id_produto,
+                            quantidade: pedido.quantidade,
+
+                            request: { //
+                                tipo: "GET",
+                                descricao: "Retorna os detalhe de um pedido específico",//documentação dos meus métodos
+                                url: "http://localhost:3000/pedidos/" + pedido.id_pedido // link para se clicar na API vai redirecionar para o GET individual do produto
+                            }
+                        }
+                    })
+                }
+                return res.status(200).send(response) // executando essa query, ela vai ser retornar minha const response
+            }
+        )
     })
+
 })
 
 
 // INSERE UM PEDIDO
 router.post("/", (req, res, next) => {
-    const pedido = { // criar o objeto e pegar irformacoes direto do body
-        id_produto: req.body.id_produto,
-        quantidade: req.body.quantidade
-    }
+    mysql.getConnection((error, conn) => { // conexão com o banco
 
-    res.status(201).send({
-        mensagem: "O pedido foi criado",
-        pedidoCriado: pedido
+    if (error) { return res.status(500).send({ error: error }) }// executando duas queries no mesmo metodo
+
+        conn.query( "SELECT * FROM  produtos WHERE id_produto = ?; ", // primeiro verifico se tem o produto
+            [req.body.id_produto],
+            (error, result, field) => { 
+                
+                if (error) { return res.status(500).send({ error: error }) }
+                if (result.length == 0) { // tratamento de erro de nao encontrar um produto que eu quero inserir
+                    return res.status(404).send({
+                        mensagem: "Produto não foi encontrado"
+                    })
+                }
+                conn.query( // se ele tenha o produto ele nao vai entrar no if
+                    "INSERT INTO pedidos (id_produto, quantidade) VALUES (?,?)", // detalhe do banco, ele só deixa criar um pedido se tiver um produto
+                    [req.body.id_produto, req.body.quantidade],
+                    (error, result, field) => {
+                        conn.release()//muito importante porque quando ele entrar na callback tem que liberar a query
+                        if (error) { return res.status(500).send({ error: error }) } // retorna um erro se nao conseguir executar a query
+
+
+                        const response = {
+                            mensagem: "Pedido inserido com sucesso",
+                            pedidoCriado: {
+                                id_pedido: result.id_pedido,
+                                id_produto: req.body.id_produto,//REFERENCES `produtos` (`id_produto`)
+                                quantidade: req.body.quantidade,
+
+                                request: {
+                                    tipo: "GET",
+                                    descricao: "Retorna todos os pedidos",
+                                    url: "http:localhost:3000/pedidos"
+                                }
+                            }
+                        }
+                        return res.status(201).send(response)
+                    }
+                )
+            })
+
     })
+
 })
 
 
 // RETORNA OS DADOS DE UM PRODUTO
 router.get("/:id_pedido", (req, res, next) => { // entra em detalhes de um unico pedido
-    const id = req.params.id_pedido // aqui iremos selecionar o id do pedido atravez dos params
+    mysql.getConnection((error, conn) => {
+        if (error) { return res.status(500).send({ error: error }) } // aqui é o callback da conexão
+        conn.query(
+            "SELECT * FROM  pedidos WHERE id_pedido = ?; ",
+            [req.params.id_pedido],
+            (error, result, fields) => {
+                if (error) { return res.status(500).send({ error: error }) }//aqui é o callback da query
 
-    if (id === "especial") { // teste de parametros
-        res.status(200).send({
-            mensagem: "detalhes do pedido",
-            id_pedido: id
-        })
-    } else {
-        res.status(200).send({
-            mensagem: "Voce passou um ID"
-        })
-    }
+                if (result.length == 0) { // tratamento de erro
+                    return res.status(404).send({
+                        mensagem: "Não foi encontrado pedido com esse ID"
+                    })
+                }
+                const response = { // Aqui posso documentar meu método Get melhor, boas praticas
+                    pedido: {
+                        id_pedido: result[0].id_pedido,
+                        id_produto: result[0].id_produto,
+                        quantidade: result[0].quantidade,
+                        request: { //
+                            tipo: "GET",
+                            descricao: "Retorna um pedido específico",//documentação dos meus métodos
+                            url: "http://localhost:3000/pedidos/"  // link para se clicar na API vai redirecionar para o GET individual do produto
+                        }
+                    }
+
+                }
+                return res.status(200).send(response) // executando essa query, ela vai ser retornar minha const response
+            }
+
+        )
+    })
 
 })
 
 //DELETA UM PEDIDO
 router.delete("/", (req, res, next) => {
-    res.status(201).send({
-        mensagem: "Pedido excluído"
+    mysql.getConnection((error, conn) => {
+        if (error) { return res.status(500).send({ error: error }) }
+        conn.query( //  a query já é um callback em si
+            "DELETE FROM pedidos WHERE id_pedido = ?", [req.body.id_pedido],
+            (error, resultado, field) => {
+                conn.release()
+                if (error) { return res.status(500).send({ error: error }) }
+                const response = {
+                    mensagem: "Pedido removido com sucesso",
+                    request: {
+                        tipo: "POST",
+                        descricao: "Insere um pedido novo",
+                        url: "http://localhost:3000/pedidos",
+                        body: {
+                            id_produto: "Number",
+                            quantidade: "Number"
+                        }
+                    }
+                }
+                res.status(202).send(response)
+            }
+        )
     })
-})
 
+})
 module.exports = router // exporta produtos com esses verbos, o POST e GET
--- routes/produtos.js
+++ routes/produtos.js
@@ -5,12 +5,27 @@ const mysql = require("../mysql").pool // o .pool é para exportar o arquivo int
 // RETORNA TODOS OS PRODUTOS
 router.get("/", (req, res, next) => { //retorna todos os produtos
     mysql.getConnection((error, conn) => {
-        if (error) { return res.status(500).send({ error: error }) }
+        if (error) { return res.status(500).send({ error: error }) } // retorna um erro se nao conseguir acessar a conexão
         conn.query(
             "SELECT * FROM produtos",
-            (error, resultado, fields) => {
+            (error, result, fields) => {
                 if (error) { return res.status(500).send({ error: error }) }
-                return res.status(200).send({ response: resultado })
+                const response = { // Aqui posso documentar meu método Get melhor, boas praticas
+                    quantidade: result.length,
+                    produtos: result.map(prod => { // no array e produtos, cada item dele vou modificar o valor, vai ter esse padrão
+                        return {
+                            id_produto: prod.id_produto,
+                            nome: prod.nome,
+                            preco: prod.preco,
+                            request: { //
+                                tipo: "GET",
+                                descricao: "Retorna o detalhe de um ID específico",//documentação dos meus métodos
+                                url: "http://localhost:3000/produtos/" + prod.id_produto // link para se clicar na API vai redirecionar para o GET individual do produto
+                            }
+                        }
+                    })
+                }
+                return res.status(200).send(response) // executando essa query, ela vai ser retornar minha const response
             }
         )
     })
@@ -20,26 +35,30 @@ router.get("/", (req, res, next) => { //retorna todos os produtos
 
 // INSERE UM PRODUTO
 router.post("/", (req, res, next) => {
-    if (error) { return res.status(500).send({ error: error }) }
+
     mysql.getConnection((error, conn) => { // conexão com o banco
+        if (error) { return res.status(500).send({ error: error }) } // retorna um erro se nao conseguir acessar a conexão
         conn.query(
             "INSERT INTO produtos (nome, preco) VALUES (?,?)",
             [req.body.nome, req.body.preco],
-            (error, resultado, field) => {
+            (error, result, field) => {
                 conn.release()//muito importante porque quando ele entrar na callback tem que liberar a query
-                if (error) {
-                    return res.status(500).send({
-                        error: error,
-                        response: null
-                    })
+                if (error) { return res.status(500).send({ error: error }) } // retorna um erro se nao conseguir executar a query
 
+                const response = {
+                    mensagem: "Produto inserido com sucesso",
+                    produtoCriado: {
+                        id_produto: result.id_produto,
+                        nome: req.body.nome,
+                        preco: req.body.preco,
+                        request: {
+                            tipo: "POST",
+                            descricao: "Insere um produto",
+                            url: "http:localhost:3000/produtos"
+                        }
+                    }
                 }
-
-                res.status(201).send({
-                    mensagem: "Produto Inserido com sucesso",
-                    id_produto: resultado.insertId // id que foi inserido lembrando que a coluna pe autoincrementável
-
-                })
+                return res.status(201).send(response)
             }
         )
     })
@@ -50,33 +69,106 @@ router.post("/", (req, res, next) => {
 
 // RETORNA OS DADOS DE UM PRODUTO
 router.get("/:id_produto", (req, res, next) => { // entra em detalhes de um unico produto
-    const id = req.params.id_produto // aqui iremos selecionar o id do prodto atravez dos params
+    //const id = req.params.id_produto // aqui iremos selecionar o id do prodto atravez dos params
+    mysql.getConnection((error, conn) => {
+        if (error) { return res.status(500).send({ error: error }) } // aqui é o callback da conexão
+        conn.query(
+            "SELECT * FROM  produtos WHERE id_produto = ?; ",
+            [req.params.id_produto],
+            (error, result, fields) => {
+                if (error) { return res.status(500).send({ error: error }) }//aqui é o callback da query
 
-    if (id === "especial") { // teste de parametros
-        res.status(200).send({
-            mensagem: "Voce descobriu o Id Especial",
-            id: id
-        })
-    } else {
-        res.status(200).send({
-            mensagem: "Voce passou um ID"
-        })
-    }
+                if (result.length == 0) { // tratamento de erro
+                    return res.status(404).send({
+                        mensagem: "Não foi encontrado produto com esse ID"
+                    })
+                }
+                const response = { // Aqui posso documentar meu método Get melhor, boas praticas
+                    produto: {
+                        id_produto: result[0].id_produto,
+                        nome: result[0].nome,
+                        preco: result[0].preco,
+                        request: { //
+                            tipo: "GET",
+                            descricao: "Retorna o produto selecionado",//documentação dos meus métodos
+                            url: "http://localhost:3000/produtos/"  // link para se clicar na API vai redirecionar para o GET individual do produto
+                        }
+                    }
+
+                }
+                return res.status(200).send(response) // executando essa query, ela vai ser retornar minha const response
+            }
+
+        )
+    })
 
 })
+
 
 // ALTERA UM PRODUTO
 router.patch("/", (req, res, next) => {
-    res.status(201).send({
-        mensagem: "Pedido alterado"
+    mysql.getConnection((error, conn) => {
+        if (error) { return res.status(500).send({ error: error }) }
+        conn.query(
+            `UPDATE produtos
+             SET nome = ?,
+              preco = ?
+               WHERE id_produto =?`,
+            [
+                req.body.nome, req.body.preco, req.body.id_produto
+            ],
+            (error, result, field) => {
+                conn.release()
+                if (error) { return res.status(500).send({ error: error }) }
+                const response = {
+                    mensagem: "Produto alterado com sucesso",
+                    produtoAtualizado: {
+                        id_produto: req.body.id_produto,
+                        nome: req.body.nome,
+                        preco: req.body.preco,
+                        request: {
+                            tipo: "PATCH",
+                            descricao: "Altera um produto",
+                            url: "http:localhost:3000/produtos/" + req.body.id_produto
+                        }
+                    }
+                }
+                return res.status(202).send(response)
+
+            }
+        )
     })
 })
 
+
+
+
 //DELETA UM PRODUTO
 router.delete("/", (req, res, next) => {
-    res.status(201).send({
-        mensagem: "Produto excluido"
+    mysql.getConnection((error, conn) => {
+        if (error) { return res.status(500).send({ error: error }) }
+        conn.query( //  a query já é um callback em si
+            "DELETE FROM produtos WHERE id_produto = ?", [req.body.id_produto],
+            (error, resultado, field) => {
+                conn.release()
+                if (error) { return res.status(500).send({ error: error }) }
+                const response = {
+                    mensagem : "Produto removido com sucesso",
+                    request : {
+                        tipo : "POST",
+                        descricao : "Insere um produto",
+                        url: "http://localhost:3000/produtos",
+                        body:{
+                            nome: "String",
+                            preco : "Number"
+                        }
+                    }
+                }
+                res.status(202).send(response)
+            }
+        )
     })
+
 })
 
 module.exports = router // exporta produtos com esses verbos, o POST e GET
